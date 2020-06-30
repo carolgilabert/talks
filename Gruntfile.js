4a4cecfc7ed2075bb1a3826a862683a7a6292d8b
Carolina Gilabert <carolgilabert@gmail.com>
2020-06-30 11:59:35 +0100

    Adding WiT deck.

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,7 +133,7 @@ module.exports = function(grunt) {
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'css/reveal.scss', 'decks/*/*.css' ],
 				tasks: 'css-core'
 			},
 			html: {
